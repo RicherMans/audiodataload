a2fa886cfc8d19512601cfdb2b2a1d9c0321e803
richermans <heini89@gmail.com>
2016-06-01 16:42:29 +0800

    Now works for any kinds of sequence length

--- Sequenceiterator.lua
+++ Sequenceiterator.lua
@@ -59,7 +59,7 @@ function Sequenceiterator:__init(...)
     end
 
 
-    local onetensor = torch.LongTensor(self.wrappedmodule.samplelengths:size(1)):fill(1)
+    local onetensor = torch.Tensor(self.wrappedmodule.samplelengths:size(1)):typeAs(self.wrappedmodule.samplelengths):fill(1)
     -- Do for each utterance the size calculation. When we sequence any utterance, it can be said that we just remove the last self.seqlen frames from this utterance
     local numsamples = torch.add(self.wrappedmodule.samplelengths,-self.seqlen)
     -- we have always at least 1 one seqence for each utterance
