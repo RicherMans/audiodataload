3d6c06c2e28a492be69367bffa12142491751ef6
Henri Dinkel <heini89@gmail.com>
2016-08-04 22:43:47 +0800

    Fixed bug when hdf5 is used with htkdataloader ( but created some other)

--- HtkDataloader.lua
+++ HtkDataloader.lua
@@ -36,13 +36,16 @@ end
 
 -- Loads the given audiofilepath and subs the given tensor to be in range start,stop. Zerotensor is returned if the stop argument is larger than the audiofile
 function HtkDataloader:loadAudioSample(audiofilepath,start,stop,...)
-    local htkbuf = _htktorch.loadsample(audiofilepath,start)
+    -- The passed start is calculated over the whole dimensions, thus we pass start equally for each iterator
+    -- the calculation just reverts the starting point calculation in getsample()
+    return _htktorch.loadsample(audiofilepath,(start-1)/self:dim() + 1)
     -- Return just a vector of zeros. This only happenes when called by Sequenceiterator, otherwise this case is nonexistent
-    if stop > htkbuf:size(1) then
-        return torch.zeros(stop-start + 1)
-    else
-        return htkbuf
-    end
+    -- TODO: Fix that problem
+    -- if stop - start > htkbuf:size(1) then
+    --     return torch.zeros(stop-start + 1)
+    -- else
+    --     return htkbuf
+    -- end
 
 end
 
@@ -67,10 +70,12 @@ function HtkDataloader:getSample(labels, ids, ...)
     local sample = nil
     -- Starting frame
     local framestart = 1
+    local frameend = -1
     -- Get the current offset for the data
     for i=1,labels:size(1) do
-        framestart = self.sampletoclassrange[ids[i]] 
-        sample = self:loadAudioSample(readfilelabel(labels[i]),framestart,-1,...)
+        framestart = (self.sampletoclassrange[ids[i]] - 1) * ( framewindow ) + 1
+        frameend = framestart+framewindow - 1
+        sample = self:loadAudioSample(readfilelabel(labels[i]),framestart,frameend,...)
         input[i]:copy(sample)
     end
     return input
